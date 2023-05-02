29f453d4d8aab5a7e809ed0304411015fb6e5db8
allen3_huang <allen3_huang@asus.com>
2023-05-02 22:02:28 +0800

    add nlog sample

---
+++ NLogSample/NLogSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogSample
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+
+            logger.Trace("trace log message");
+            logger.Debug("debug log message");
+            logger.Info("info log message");
+            logger.Warn("warn log message");
+            logger.Error(new Exception("exception test"), "error log message");
+            logger.Fatal("fatal log message");
+
+            Console.ReadLine();
+        }
+
+    }
+}
